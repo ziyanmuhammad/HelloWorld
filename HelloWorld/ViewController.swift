5ecc6be0e7350f56a680e5ca56262d65647bbe7b
ziyanmuhammad <ziyanaqsha@gmail.com>
2021-04-23 14:30:56 +0700

    hello world

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,8 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        helloWorld()
     }
-
+    func helloWorld(){
+        print("HelloWorld")
+    }
 
 }
 
